0570c628450d83de9a13f0fa34b730d2e1b0a4ed
NwaObed <nwachukwobed@gmail.com>
2023-07-13 16:53:30 -0700

    count data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@ long int longintType;
 long long int longlongintType;
 float floatType;
 // sizeof evaluates the size of a variable
-printf("Size of char: %zu byte(s)\n", sizeof(charType));
-printf("Size of int: %zu bytes(s)\n", sizeof(intType));
-printf("Size of long int: %zu byte(s)\n", sizeof(longintType));
-printf("Size of long long int: %zu byte(s)\n", sizeof(longlongintType));
-printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu bytes(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return 0;
 }
