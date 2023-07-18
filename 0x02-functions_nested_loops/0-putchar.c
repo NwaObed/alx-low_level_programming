79fdc5d98abbe970ff7382d5638d965a3ebafd11
NwaObed <nwachukwobed@gmail.com>
2023-07-18 19:41:32 +0000

    betty style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,10 @@
 #include "main.h"
 int main()
+/**
+ * Program to print char at a time
+ * 
+ * Return 0
+ */
 {
 char test[] = "_putchar\n";
 int i;
@@ -7,6 +12,6 @@ for (i = 0; test[i] != '\0'; i++)
 {
 _putchar(test[i]);
 }
-return 0;
+return (0);
 }
 
