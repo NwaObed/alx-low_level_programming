77e36d2db33bb08868e1b9a38c7254ed96739ce3
NwaObed <nwachukwobed@gmail.com>
2023-07-18 22:11:39 +0000

    task 10 betty

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,11 @@
+#include "main.h"
+/**
+ * times_table - Print 9-times table
+ * Return: Always 0 (Success)
+ */
 int add(int num1, int num2)
 {
-    int sum;
-    sum = num1 + num2;
-    return (sum);
+int sum;
+sum = num1 + num2;
+return (sum);
 }
