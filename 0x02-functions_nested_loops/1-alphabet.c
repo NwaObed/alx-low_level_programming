258bce9d0977bf69a208b2c4056b0394e4e2a8fa
NwaObed <nwachukwobed@gmail.com>
2023-07-18 21:19:10 +0000

    betty task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - Prints the alphabet in lowercase
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
