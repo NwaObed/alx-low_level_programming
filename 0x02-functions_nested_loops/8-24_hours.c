70efd196d29b948099133fbe65c0ce5be13eb981
NwaObed <nwachukwobed@gmail.com>
2023-07-18 23:33:31 +0000

    task 8 betty

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,24 +6,24 @@
  */
 void jack_bauer(void)
 {
-int hr = 24;
-int minute = 60;
-int i = 0;
-while (i < hr)
-{
-int j = 0;
-while (j < minute)
-{
-if ((i < 10) & (j < 10))
-printf("0%d:%d0\n", i, j);
-else if ((i < 10) & (j >= 10))
-printf("0%d:%d\n", i, j);
-else if ((i >= 10) & (j < 10))
-printf("%d:0%d\n", i, j);
-else
-printf("%d:%d\n", i, j);
-j++;
-}
-i++;   
-}
+    int hr = 24;
+    int minute = 60;
+    int i = 0;
+    while (i < hr)
+    {
+        int j = 0;
+        while (j < minute)
+        {
+            if ((i < 10) & (j < 10))
+                printf("0%d:%d%d\n", i, j, j);
+            else if ((i < 10) & (j >= 10))
+                printf("0%d:%d\n", i, j);
+            else if ((i >= 10) & (j < 10))
+                printf("%d:0%d\n", i, j);
+            else
+                printf("%d:%d\n", i, j);
+            j++;
+        }
+        i++;
+    }
 }
