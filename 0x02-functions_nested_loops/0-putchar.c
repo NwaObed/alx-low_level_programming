dbf3ce4936980532994b4862eb47cafaa783b7c3
NwaObed <nwachukwobed@gmail.com>
2023-07-18 21:04:53 +0000

    betty please

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-    char test[] = "_putchar";
-    int i;
-    for (i = 0; test[i] != '\0'; i++)
-    _putchar(test[i]);
-    _putchar('\n');
-    return (0);
+char test[] = "_putchar";
+int i;
+for (i = 0; test[i] != '\0'; i++)
+_putchar(test[i]);
+_putchar('\n');
+return (0);
 }
