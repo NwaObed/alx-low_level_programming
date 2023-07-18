51766d7762928769318075650b252e5bc6aa08c6
NwaObed <nwachukwobed@gmail.com>
2023-07-18 21:15:32 +0000

    betty task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,12 @@
 /**
  * main - Entry point
  *
- * Return: void
+ * Return: 0
  */
 void print_alphabet(void)
 {
 char letter;
-for(letter = 'a'; letter <= 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 _putchar('\n');
 }
