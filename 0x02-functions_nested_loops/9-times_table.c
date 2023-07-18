c25adf0df08113f7180dd7d9bb901c053c678ccf
NwaObed <nwachukwobed@gmail.com>
2023-07-18 22:54:46 +0000

    task 9 betty

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,19 +9,18 @@ void times_table(void)
 int i = 0;
 while (i <= 9)
 {
-    int j = 0;
-    int val;
-    while (j <= 9)
-    {
-        val = i * j;
-        
-        if (j != 9)
-            printf("%2d, ",val);
-        else
-            printf("%2d\n",val);
-        j++;
-    }
-    i++;
+int j = 0;
+int val;
+while (j <= 9)
+{
+val = i * j;
+
+if (j != 9)
+printf("%2d, ",val);
+else
+printf("%2d\n",val);
+j++;
 }
-_putchar('\n');
+i++;
+}
 }
