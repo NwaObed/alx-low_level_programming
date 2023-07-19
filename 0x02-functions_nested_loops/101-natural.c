f8de10d6cf783209e5363836296f5fc29d13a9f5
NwaObed <nwachukwobed@gmail.com>
2023-07-18 18:33:40 -0700

    update remote

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+* main : Entry point
+* print_mult_3_or_5 - Print multiples of 3 or 5
+* Return: 0
+*/
+int main(void)
+{
+print_mult_3_or_5();
+}
 void print_mult_3_or_5(void)
 {
-  int i;
-  int sum = 0;
-  for (i = 0; i < 1024; i++)
-    {
-      if ((i % 3 == 0) || (i % 5 == 0))
-	sum += i;
-    }
+int i;
+int sum = 0;
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+sum += i;
+}
 }
