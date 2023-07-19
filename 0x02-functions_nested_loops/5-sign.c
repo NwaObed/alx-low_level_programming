155a78d2f906216b56669de639d999de9c6c5806
NwaObed <nwachukwobed@gmail.com>
2023-07-18 17:52:50 -0700

    task 5 correction

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,21 +1,25 @@
 #include "main.h"
-
+/**
+ * print_sign - Print sign of a number
+ * @n: The number
+ * Return: Always 0 (Success)
+ */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return (0);
-    }
-    else
-    {
-        _putchar('-');
-        _putchar('1');
-        return (-1);
-    }
+  if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
 }
