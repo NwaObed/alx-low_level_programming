2ddb428484902cd172733ea4ae249d2b5fa4311d
NwaObed <nwachukwobed@gmail.com>
2023-07-20 23:09:34 +0000

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,19 +4,19 @@
  * @n: the number of lines
  */
 void print_square(int n){
-    if (n <= 0)
-        _putchar('\n');
-    else
-        {
-            int i;
-            int j;
-            for (i = 0; i < n; i++)
-            {
-                for (j = 0; j < n; j++)
-                {
-                    _putchar(35);
-                }
-                _putchar('\n');                
-            }    
-        }
+if (n <= 0)
+_putchar('\n');
+else
+{
+int i;
+int j;
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+_putchar(35);
+}
+_putchar('\n');                
+}    
+}
 }
