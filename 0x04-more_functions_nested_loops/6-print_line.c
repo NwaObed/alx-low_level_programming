5f1b94df992ff51cd6cc0b21dc40a59900680500
NwaObed <nwachukwobed@gmail.com>
2023-07-20 21:47:42 +0000

    task 3

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,12 +6,15 @@
  */
 void print_line(int n)
 {
-    if (n <= 0)
-        _putchar('\n');
-    else
-        {
-            while (n > 0)
-                _putchar('_');
-            _putchar('\n');
-        }
+if (n <= 0)
+_putchar('\n');
+else
+{
+while (n > 0)
+{
+_putchar('_');
+n--;
+}
+_putchar('\n');
+}
 }
