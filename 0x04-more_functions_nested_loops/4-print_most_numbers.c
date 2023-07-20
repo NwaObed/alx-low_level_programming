70053163dc0f9de94d67ef55d27bf65bcce4370d
NwaObed <nwachukwobed@gmail.com>
2023-07-20 17:12:23 +0000

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,3 +1,17 @@
 #include "main.h"
-
-
+/**
+ * print_most_numbers - Prints more numbers
+ * * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+int i = 0;
+while (i < 0)
+{
+if ((i == 2) || (i == 4))
+continue;
+else
+_putchar(i);
+}
+_putchar('\n');
+}
