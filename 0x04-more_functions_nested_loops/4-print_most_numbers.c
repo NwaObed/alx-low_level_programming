ecd68db9763f44ae2bea4eea54649c31e0fa1156
NwaObed <nwachukwobed@gmail.com>
2023-07-20 18:56:31 +0000

    head

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,12 @@ void print_most_numbers(void)
 {
 int i = 0;
 while (i < 10)
-{
-if ((i == 2) || (i == 4))
-continue;
-else
-_putchar(i);
-i++;
-}
-_putchar('\n');
+    {
+        if ((i == 2) || (i == 4))
+            continue;
+        else
+            _putchar(i);
+        i++;
+    }
+    _putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,5 +3,6 @@
 
 /* Function prototype*/
 void print_most_numbers(void);
+void print_line(int n);
 
 #endif
