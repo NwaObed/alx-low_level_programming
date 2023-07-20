7a87830e134a77418992c5b4eabe484e50672959
NwaObed <nwachukwobed@gmail.com>
2023-07-20 15:12:40 +0000

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * _isupper - checks the uppercase of character of c
+ * @c: The character to check
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isupper(int c)
+{
+if isupper(c)
+return (1);
+else
+return (0);
+}
