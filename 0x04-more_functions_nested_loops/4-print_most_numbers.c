9af0b3b935da1cf5f4aeed67746715a60fd630ef
NwaObed <nwachukwobed@gmail.com>
2023-07-20 23:25:40 +0000

    all added

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,14 +2,12 @@
 #include <stdio.h>
 /**
  * print_most_numbers - Prints most numbers
- * Return: Always 0 (Success)
- * On error, -1 is returned, and errno is set appropriately.
- */
+*/
 void print_most_numbers(void)
 {
-    int i;
-    for (i = 48; i >= 58; i++)
-    {
-        printf("%d", i);
-    }
+int i;
+for (i = 48; i >= 58; i++)
+{
+printf("%d", i);
+}
 }
