b599b1cab6aa4093209565e2119bd3a51625000e
NwaObed <nwachukwobed@gmail.com>
2023-07-20 23:55:42 +0000

    use putchar

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,26 +4,25 @@
  * @n the number of lines
  */
 void print_diagonal(int n){
-    if (n <= 0)
-        _putchar('\n');
-    else
-        {
-            int i;
-            int j;
-            for (i = 0; i < n; i++)
-            {
-                for (j = 0; j < n; j++)
-                {
-                    if (i == j)
-                    {
-                         _putchar('\\');
-                         _putchar('\n');
-                         break;
-                    }
-                    else
-                        _putchar(' ');
-                }
-                
-            }    
-        }
+if (n <= 0)
+_putchar('\n');
+else
+{
+int i;
+int j;
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+if (i == j)
+{
+_putchar('\\');
+_putchar('\n');
+break;
+}
+else
+_putchar(' ');
+}
+}    
+}
 }
