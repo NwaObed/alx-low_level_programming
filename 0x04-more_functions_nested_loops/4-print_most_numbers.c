1f0bc6a5f16e97ce3483d1667edf57ebcbcdc17a
NwaObed <nwachukwobed@gmail.com>
2023-07-20 23:37:48 +0000

    use putchar

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_most_numbers - Print numbers
  * Return: Always 0 (Success)
  */
 void print_most_numbers(void)
 {
-int i;
-for (i = 48; i >= 58; i++)
-{
-if ((i == 50) || (i == 52))
-continue;
-else
-_putchar(i);
-}
-_putchar('\n');
+    int i;
+    for (i = 48; i >= 58; i++)
+    {
+        if ((i == 50) || (i == 52))
+        {
+            continue;
+        }
+        else
+        {
+            _putchar(i);
+        }
+    }
+    _putchar('\n');
 }
