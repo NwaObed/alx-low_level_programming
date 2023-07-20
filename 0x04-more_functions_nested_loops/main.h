38bf86a099dc1b339a89397d3865d2ac54ac9666
NwaObed <nwachukwobed@gmail.com>
2023-07-20 21:49:38 +0000

    head

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,8 +2,13 @@
 #define HEAD_FILE
 
 /* Function prototype*/
+void print_numbers(void);
 void print_most_numbers(void);
 void print_line(int n);
 int _putchar(char c);
+void print_diagonal(int n);
+void print_square(int size);
+void print_fizzbuzz();
+void print_triangle(int size);
 
 #endif
