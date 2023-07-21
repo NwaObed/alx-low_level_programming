a6314457fa898025c74b42280f16dfdf5d370ce8
NwaObed <nwachukwobed@gmail.com>
2023-07-21 00:01:48 +0000

    use putchar

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,13 +3,13 @@
  * print_diagonal : draw a diagonal line
  * @n the number of lines
  */
-void print_diagonal(int n){
+void print_diagonal(int n)
+{
 if (n <= 0)
 _putchar('\n');
 else
 {
-int i;
-int j;
+int i, j;
 for (i = 0; i < n; i++)
 {
 for (j = 0; j < n; j++)
