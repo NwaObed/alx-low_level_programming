7d8a810dc5638ba42de6be773a1243cfa7b36ca4
NwaObed <nwachukwobed@gmail.com>
2023-07-21 00:24:10 +0000

    use putchar

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,16 +7,19 @@
  */
 void print_triangle(int size)
 {
-int i;
-int j;
+int i, j;
 for (i = (size - 1); i >= 0; i--)
 {
 for (j = 0; j < size; j++)
 {
 if ( j >= i)
+{
 _putchar('#');
+}
 else
+{
 _putchar(' ');
+}
 }
 _putchar('\n');
 }
