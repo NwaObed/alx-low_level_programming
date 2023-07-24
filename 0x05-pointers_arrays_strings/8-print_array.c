ba230dd567b889cd864c267c362c197198bc9b71
NwaObed <nwachukwobed@gmail.com>
2023-07-24 14:48:35 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_array - Print elements of array
+ * @a: Pointer to an array of integers
+ * @n: number of elements
+ * Return: 0 on success
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	while (i < n)
+	{
+		printf("%d, ", a[i]);
+	}
+}
+
