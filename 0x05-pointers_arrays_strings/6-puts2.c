401aa79ad503c10c2b1a40cd9b607526c4ee222f
NwaObed <nwachukwobed@gmail.com>
2023-07-24 12:22:17 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - Prints every other character of a string
+ * @str: Pointer to the string to be printed
+ * Return: 0 on success
+ */
+void puts2(char *str)
+{
+	str_len = strlen(str);
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
+
