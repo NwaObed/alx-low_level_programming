f9733b2954e939a9dd8d5dbfe38cda3285a1dca2
NwaObed <nwachukwobed@gmail.com>
2023-07-24 16:23:52 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,13 @@
 void puts2(char *str)
 {
 	int i = 0;
-	
+	int j = 0;
+
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		_putchar(str[j]);
+		i++;
+		j += 2;
 	}
 	_putchar('\n');
 }
