c7654d0c20cf1012bbaaad0fb3d559277b47db71
NwaObed <nwachukwobed@gmail.com>
2023-07-24 14:58:26 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,12 +9,18 @@
 void print_array(int *a, int n)
 {
 	int i;
-	
 	i = 0;
 	while (i < n)
 	{
-		printf("%d, ", a[i]);
-		i++;
+		if (i = (n - 1))
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+			i++;
+		}
 	}
 }
 
