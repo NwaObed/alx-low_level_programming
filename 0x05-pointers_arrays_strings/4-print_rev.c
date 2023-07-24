6a382d23130bbc9f17d75a7d45aea593964eb2e8
NwaObed <nwachukwobed@gmail.com>
2023-07-24 11:32:02 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - Print str in reverse order
+ * @s: Pointer to the string to be printed
+ * Return: 0 (Success)
+ */
+void print_rev(char *s)
+{
+	int length;
+	char *rev_s;
+	int i;
+
+	for (i = 0; i < length; i++)
+		_putchar(s[length - 1 - i]);
+}
+
