bd24da6a42272a2f424e8a3df67f7775381db047
NwaObed <nwachukwobed@gmail.com>
2023-07-24 15:11:17 -0700

    one extra char removed

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,7 +16,7 @@ void puts_half(char *str)
 	{
 		half_len = (str_len - 1) / 2;
 		i = 0;
-		for (i = half_len; str[i] != '\0'; i++)
+		for (i = (half_len - 1); str[i] != '\0'; i++)
 		{
 			_putchar(str[i]);
 		}
