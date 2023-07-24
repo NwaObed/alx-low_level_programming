f75baf9559c6dad8df809d592fc632f85fd148a1
NwaObed <nwachukwobed@gmail.com>
2023-07-24 10:01:00 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * void reset_to_98 - Function that updates the value of a pointer
+ * @n - pointer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef HEAD_FILE
+#define HEAD_FILE
+
+/* Function Prototypes */
+void reset_to_98(int *n);
+
+#endif
