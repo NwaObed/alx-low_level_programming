71777958d1204a6425334a777dd566540e9ec86d
NwaObed <nwachukwobed@gmail.com>
2023-07-24 11:00:30 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,14 @@
 #include "main.h"
 #include <string.h>
 /**
- * int _strlen - To calculate the length of string pointed to by s
+ * _strlen - To calculate the length of string pointed to by s
  * @s: pointer
  * Return - length of str
  */
 int _strlen(char *s)
 {
 	int str_len;
+
 	str_len = strlen(s);
 	return (str_len);
 }
