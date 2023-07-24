6df9a31483c044cfa88dc1133f3e4d526baf2793
NwaObed <nwachukwobed@gmail.com>
2023-07-24 12:09:49 -0700

    task 5 - reverse str

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,11 +8,14 @@
 void rev_string(char *s)
 {
 	int str_len = strlen(s);
-	char *rev_s;
-	int i;
+	int i, j;
+	char temp;
 
-	for (i = 0; i < str_len; i++)
-		rev_s[i] = s[str_len - 1 - i];
-	*s = *rev_s;
+	for (i = 0, j = str_len - 1; i < j; i++, j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
 }
 
