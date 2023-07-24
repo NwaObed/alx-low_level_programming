7e47555eee550b6c2117b7f7a509da12fd73130f
NwaObed <nwachukwobed@gmail.com>
2023-07-24 11:51:36 -0700

    task 5 - reverse str

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - Reverse a string
+ * @s: Pointer to the string to be reversed
+ * Return: 0 on success
+ */
+void rev_string(char *s)
+{
+	int str_len = strlen(s);
+	char *rev_s;
+	int i;
+
+	for (i = 0; i < str_len; i++)
+		rev_s[i] = s[str_len - 1 - i];
+	*s = rev_s;
+}
+
