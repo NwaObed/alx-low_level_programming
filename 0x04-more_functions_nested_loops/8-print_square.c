efd93919edf6fac26dbbf447fe17c744690e1aba
NwaObed <nwachukwobed@gmail.com>
2023-07-25 15:45:10 +0000

    betty is cool

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
- * print_square : Draw a square
+ * print_square - Draw a square
  * @n: the number of lines
  */
 void print_square(int n)
 {
-if (n <= 0)
-_putchar('\n');
-else
-{
-int i, j;
-for (i = 0; i < n; i++)
-{
-for (j = 0; j < n; j++)
-{
-_putchar('#');
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
 }
-_putchar('\n');                
-}    
-}
-}
+
