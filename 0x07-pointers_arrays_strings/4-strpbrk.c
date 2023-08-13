4c0e25fd65c0c4ec50fc123b9047f111ad79ed48
NwaObed <nwachukwobed@gmail.com>
2023-08-13 12:23:19 +0000

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
   * _strpbrk - Locate the first occurrence of byte in a string
   * @s: First string
@@ -8,14 +9,20 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
-	char *ptr = NULL;
+	char *ptr;
 
-	for (i = 0; ; i++; j = 0; j++)
+	for (i = 0; ; i++;)
 	{
-		if (s[i] == accept[j])
+		for (j = 0; j < strlen(accept); j++)
 		{
-			ptr = &s[i];
-			break;
+
+			if (s[i] == accept[j])
+			{
+				ptr = &s[i];
+				break;
+			}
+			else
+				ptr = NULL;
 		}
 	}
 	return (ptr);
