8edb8cc6c71a73bab564eb6ea7b38a2ded898b9c
NwaObed <nwachukwobed@gmail.com>
2023-08-14 18:38:40 +0000

    betty - struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,11 @@
 #define DOG_H
 /**
   * struct dog - Dog structure
+  * @name: Pointer to dog name
+  * @age: Age of dog
+  * @owner: Pointer to dog owner
+  *
+  * Description: Structure to create a dog object
   */
 struct dog
 {
