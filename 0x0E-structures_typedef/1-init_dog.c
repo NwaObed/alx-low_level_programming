d4a53a2fca4462e0a6316fb27b47d96d347a024d
NwaObed <nwachukwobed@gmail.com>
2023-08-15 17:28:46 +0000

    init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,6 @@
 /**
   * init_dog - Initialize a variable of type struct dog
   * @d: Pointer to the struct dog
-  * @dog: Dog struct
   * @name: Pointer to the dog name
   * @age: Dog age
   * @owner: Dog owner
