d21c47d0829b641d33f8b4652354a83b69c3725f
NwaObed <nwachukwobed@gmail.com>
2023-08-28 06:59:23 +0000

    task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,16 +2,16 @@
 
 /**
  * free_listint - Frees a listint_t list.
- * @head: A pointer to the head of the listint_t list to be freed.
+ * @head: Pointer to the head of the listint_t list to be freed.
  */
 void free_listint(listint_t *head)
 {
-	listint_t *tmp;
+	listint_t *var;
 
 	while (head)
 	{
-		tmp = head->next;
+		var = head->next;
 		free(head);
-		head = tmp;
+		head = var;
 	}
 }
