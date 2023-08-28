43c4ac3ef41cee269ab666d345a909e63523e1d2
NwaObed <nwachukwobed@gmail.com>
2023-08-28 06:48:59 +0000

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,9 +2,8 @@
 #include <stdio.h>
 
 /**
- * listint_len - Returns the number of elements
- *               in a linked listint_t list.
- * @h: A pointer to the head of the listint_t list.
+ * listint_len - Returns number of elements in linked listint_t list.
+ * @h: Pointer to the head of the listint_t list.
  *
  * Return: The number of elements in the listint_t list.
  */
