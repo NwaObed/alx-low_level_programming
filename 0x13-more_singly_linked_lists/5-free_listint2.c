2ef238fe964b0a1dccab330fa11b510eed3d8ed4
NwaObed <nwachukwobed@gmail.com>
2023-08-28 19:01:37 +0000

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,23 +2,22 @@
 
 /**
  * free_listint2 - Frees a listint_t list.
- * @head: A pointer to the address of the
- *        head of the listint_t list.
+ * @head: Pointer to the head of the listint_t list.
  *
  * Description: Sets the head to NULL.
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *var;
 
 	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		tmp = (*head)->next;
+		var = (*head)->next;
 		free(*head);
-		*head = tmp;
+		*head = var;
 	}
 
 	head = NULL;
