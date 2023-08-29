14e4500fa890779c34699effde9ac11ce741eb3d
NwaObed <nwachukwobed@gmail.com>
2023-08-29 22:08:56 +0000

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	orig->next = dup->next;
-	dup->next = new;
+	dup->next = orig;
 
-	return (new);
+	return (orig);
 }
