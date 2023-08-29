6d5dcfc69a19c2a1bb6eacb29bbbf0586595e692
NwaObed <nwachukwobed@gmail.com>
2023-08-29 22:05:13 +0000

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *dup = *head;
+	listint_t *orig, *dup = *head;
 	unsigned int node;
 
 	orig = malloc(sizeof(listint_t));
