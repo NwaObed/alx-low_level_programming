f6c493ef21631d2d69a6668c30c936d56f2c95be
NwaObed <nwachukwobed@gmail.com>
2023-08-31 22:18:28 +0000

    header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,10 +2,8 @@
 #define MAIN_H
 
 /*
- * File: alx
- * AUth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
+ * AUth: Obed Nwachukwu 
+ * Desc: Header file for 0x13-bit_manipulation directory.
  */
 
 int _putchar(char c);
